bbcd4c929250839698c9eb403d3a1b8a225bc725
Yargi <markus.krusch@gmail.com>
2022-09-14 19:43:29 +0200

    added resources and changes in Dijkstra.java

--- _01_Graphs_Topologica_Sorting_MST/src/main/java/Dijkstra.java
+++ _01_Graphs_Topologica_Sorting_MST/src/main/java/Dijkstra.java
@@ -1,8 +1,54 @@
-import java.util.List;
+import java.util.*;
 
 public class Dijkstra {
 
     public static List<Integer> dijkstraAlgorithm(int[][] graph, int sourceNode, int destinationNode) {
-        return null;
+
+        int[] distances = new int[graph.length];
+        Arrays.fill(distances, Integer.MAX_VALUE);
+
+        int[] previous = new int[graph.length];
+        Arrays.fill(previous, -1);
+
+        boolean[] visited = new boolean[graph.length];
+
+
+        distances[sourceNode] = 0;
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.comparingInt(node -> distances[node]));
+
+        queue.offer(sourceNode);
+
+        while (!queue.isEmpty()) {
+            int parent = queue.poll();
+            visited[parent] = true;
+            int[] children = graph[parent];
+
+            for (int childNode = 0; childNode < children.length; childNode++) {
+                if (children[childNode] != 0 && !visited[childNode]) {
+                    queue.offer(childNode);
+
+                    int distance = distances[parent] + graph[parent][childNode];
+
+                    if (distance < distances[childNode]) {
+                        distances[childNode] = distance;
+                        previous[childNode] = parent;
+                    }
+                }
+            }
+        }
+
+        List<Integer> path = new ArrayList<>();
+
+        int n = previous[destinationNode];
+
+        while (n != -1) {
+            path.add(n);
+            n = previous[n];
+        }
+
+        Collections.reverse(path);
+
+        return path;
     }
 }
