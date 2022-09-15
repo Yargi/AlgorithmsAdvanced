d53e1dd14e247dfc3a8415a686696948ab3328c0
Yargi <markus.krusch@gmail.com>
2022-09-15 03:10:13 +0200

    edited KruskalAlgorithm.java

--- _01_Graphs_Topologica_Sorting_MST/src/main/java/KruskalAlgorithm.java
+++ _01_Graphs_Topologica_Sorting_MST/src/main/java/KruskalAlgorithm.java
@@ -1,9 +1,23 @@
-import java.util.List;
+import java.util.*;
 
 public class KruskalAlgorithm {
 
     public static List<Edge> kruskal(int numberOfVertices, List<Edge> edges) {
-        return null;
+
+        PriorityQueue<Edge> orderedEdges = new PriorityQueue<>(edges);
+
+        List<Edge> forest = new ArrayList<>();
+
+        while (!orderedEdges.isEmpty()) {
+            Edge edge = orderedEdges.poll();
+
+            int source = edge.getStartNode();
+            int destination = edge.getEndNode();
+
+            int[] parents = new int[numberOfVertices];
+        }
+
+        return forest;
     }
 
     public static int findRoot(int node, int[] parents) {
