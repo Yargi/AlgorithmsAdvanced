094f3f05b8f87cecb2fe2a9a48cbfafdfef26ec1
Yargi <markus.krusch@gmail.com>
2022-10-12 12:09:06 +0200

    changes in project structure

--- _02_Graphs_Bellman-Ford_-_Longest_Path_in_DAG_-_Lab/_02_Longest_Path/src/main/java/Main.java
+++ _02_Graphs_Bellman-Ford_-_Longest_Path_in_DAG_-_Lab/_02_Longest_Path/src/main/java/Main.java
@@ -1,76 +1,114 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
-    private static List<Edge> graph = new ArrayList<>();
+    public static int[][] graph;
+    public static int[] distance;
+    public static boolean[] visited;
 
-    private static int[] distance;
-    private static boolean[] visited;
+    public static void main(String[] args) {
 
-    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
 
-        BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+        int nodes = Integer.parseInt(sc.nextLine());
+        int edges = Integer.parseInt(sc.nextLine());
 
-        int vertices = Integer.parseInt(rd.readLine());
-        int edges = Integer.parseInt(rd.readLine());
+        graph = new int[nodes + 1][nodes + 1];
 
         for (int i = 0; i < edges; i++) {
-            int[] tokens = Arrays.stream(rd.readLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+
+            int[] tokens = Arrays.stream(sc.nextLine().split("\\s+"))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+
             int source = tokens[0];
             int destination = tokens[1];
             int weight = tokens[2];
-            graph.add(new Edge(weight, source, destination));
+
+            graph[source][destination] = weight;
         }
 
-        int source = Integer.parseInt(rd.readLine());
-        int destination = Integer.parseInt(rd.readLine());
+        int source = Integer.parseInt(sc.nextLine());
+        int destination = Integer.parseInt(sc.nextLine());
 
-        rd.close();
+        sc.close();
 
-        distance = new int[graph.size()];
-        visited = new boolean[graph.size()];
-        ArrayDeque<Edge> sorted = new ArrayDeque<>();
+        distance = new int[graph.length];
+        Arrays.fill(distance, Integer.MIN_VALUE);
+        distance[source] = 0;
+        visited = new boolean[graph.length];
 
-        //graph.sort(Comparator.comparing(Edge::getWeight).reversed());
-        for (Edge edge :
-                graph) {
-            topologicalSort(edge.getSource(), sorted);
+        ArrayDeque<Integer> sorted = new ArrayDeque<>();
+
+        for (int i = 1; i < graph.length; i++) {
+
+            topologicalSort(i, sorted);
         }
 
-        System.out.println();
+        while (!sorted.isEmpty()) {
 
+            int node = sorted.pop();
+            for (int i = 1; i < graph[node].length; i++) {
+                int weight = graph[node][i];
+                if (weight != 0) {
+                    if (distance[node] + weight > distance[i]) {
+                        distance[i] = distance[node] + weight;
+                    }
+                }
+            }
+        }
+
+        System.out.println(distance[destination]);
     }
 
-    private static void topologicalSort(int node, ArrayDeque<Edge> sorted) {
+    private static void topologicalSort(int node, ArrayDeque<Integer> sorted) {
 
-    }
+        if (visited[node]) {
 
-    private static class Edge {
-
-        private int weight;
-        private int source;
-        private int destination;
-
-
-        public Edge(int weight, int source, int destination) {
-            this.weight = weight;
-            this.source = source;
-            this.destination = destination;
+            return;
         }
 
-        public int getWeight() {
-            return weight;
+        visited[node] = true;
+
+        for (int i = 1; i < graph[node].length; i++) {
+
+            if (graph[node][i] != 0) {
+                topologicalSort(i, sorted);
+            }
         }
 
-        public int getSource() {
-            return source;
-        }
-
-        public int getDestination() {
-            return destination;
-        }
+        sorted.push(node);
     }
 }
+
+/*
+// 21
+6
+11
+1 2 5
+1 3 3
+2 4 6
+2 3 2
+3 5 4
+3 6 2
+3 4 7
+4 6 1
+4 5 3
+5 6 4
+6 2 1
+1
+6
+
+//13
+4
+4
+1 2 5
+1 3 3
+3 4 6
+4 2 4
+1
+2
+
+ */
